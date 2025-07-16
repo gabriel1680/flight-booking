a333c164764577b5d05b2295ba0d6ff57c57f659
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-07-16 14:25:48 -0300

    refactor(admin): rename HttpServerMain to SpringRestApplication

--- src/main/java/org/gbl/flight_admin/in/http/SpringRestApplication.java
+++ src/main/java/org/gbl/flight_admin/in/http/SpringRestApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HttpServerMain {
+public class SpringRestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HttpServerMain.class, args);
+        SpringApplication.run(SpringRestApplication.class, args);
     }
 }
