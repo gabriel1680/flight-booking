a89a8a7d3f87bc8ea5dc00d032381bc093dc0c43
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-07-18 09:29:01 -0300

    feat(admin): update configurations

--- src/main/java/org/gbl/flight_admin/config/DevConfiguration.java
+++ src/main/java/org/gbl/flight_admin/config/DevConfiguration.java
@@ -1,20 +1,22 @@
 package org.gbl.flight_admin.config;
 
 import org.gbl.flight_admin.FlightAdminApi;
-import org.gbl.flight_admin.app.domain.Flight;
 import org.gbl.flight_admin.FlightAdminFacade;
+import org.gbl.flight_admin.app.domain.Flight;
 import org.gbl.flight_admin.app.service.FlightQueryService;
 import org.gbl.flight_admin.app.service.FlightRepository;
 import org.gbl.flight_admin.out.memory.InMemoryFlightRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
-public class CoreConfiguration {
+@Profile("development")
+public class DevConfiguration {
 
     private final List<Flight> flights = new ArrayList<>();
 
