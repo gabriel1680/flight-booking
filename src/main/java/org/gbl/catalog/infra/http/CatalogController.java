0923af9bf39add1b190a803e3a954b3d906778e6
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-07-25 17:27:29 -0300

    feat(catalog): add CatalogController.getFlightOf

--- src/main/java/org/gbl/catalog/infra/http/CatalogController.java
+++ src/main/java/org/gbl/catalog/infra/http/CatalogController.java
@@ -1,6 +1,7 @@
 package org.gbl.catalog.infra.http;
 
 import org.gbl.catalog.CatalogApi;
+import org.gbl.catalog.dto.CatalogDto.GetFlightCatalogDto;
 import org.gbl.catalog.dto.CatalogDto.Pagination;
 import org.gbl.catalog.dto.CatalogDto.SearchFilter;
 import org.gbl.catalog.dto.CatalogDto.SearchFlightsCatalogQuery;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,5 +34,11 @@ public class CatalogController {
                 pageable.getSort().toString(),
                 new SearchFilter(origin, destination));
         return ResponseEntity.ok(catalogApi.searchFlights(query));
+    }
+
+    @GetMapping("/{flightId}")
+    public ResponseEntity<GetFlightCatalogDto> getFlightOf(
+            @PathVariable("flightId") String flightId) {
+        return ResponseEntity.ok(catalogApi.getFlight(flightId));
     }
 }
