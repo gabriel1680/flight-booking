4a482bf8be031e63e8f5bf611f059103946defcb
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-07-25 17:25:30 -0300

    feat(catalog): add CatalogController.getFlights

---
+++ src/main/java/org/gbl/catalog/infra/http/CatalogController.java
@@ -0,0 +1,36 @@
+package org.gbl.catalog.infra.http;
+
+import org.gbl.catalog.CatalogApi;
+import org.gbl.catalog.dto.CatalogDto.Pagination;
+import org.gbl.catalog.dto.CatalogDto.SearchFilter;
+import org.gbl.catalog.dto.CatalogDto.SearchFlightsCatalogQuery;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/catalog")
+public class CatalogController {
+
+    private final CatalogApi catalogApi;
+
+    public CatalogController(CatalogApi catalogApi) {
+        this.catalogApi = catalogApi;
+    }
+
+    @GetMapping
+    public ResponseEntity<Pagination> getFlights(
+            @Param("origin") String origin,
+            @Param("destination") String destination,
+            Pageable pageable) {
+        final var query = new SearchFlightsCatalogQuery(
+                pageable.getPageNumber(),
+                pageable.getPageSize(),
+                pageable.getSort().toString(),
+                new SearchFilter(origin, destination));
+        return ResponseEntity.ok(catalogApi.searchFlights(query));
+    }
+}
