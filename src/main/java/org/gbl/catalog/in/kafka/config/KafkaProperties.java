ca66ed8adfb4935cab5d44a5ddaa520b2ce02397
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-07-29 16:47:55 -0300

    fix(catalog): remove @Configuration annotation from KafkaProperties

--- src/main/java/org/gbl/catalog/in/kafka/config/KafkaProperties.java
+++ src/main/java/org/gbl/catalog/in/kafka/config/KafkaProperties.java
@@ -1,8 +1,7 @@
 package org.gbl.catalog.in.kafka.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "kafka")
-public record KafkaProperties(String bootstrapServers, int poolTimeout, boolean autoCreateTopics) {}
+public record KafkaProperties(String bootstrapServers, int poolTimeout, boolean autoCreateTopics) {
+}
