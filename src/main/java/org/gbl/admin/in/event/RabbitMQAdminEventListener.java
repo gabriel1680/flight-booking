3e9d163be4b12bc917524df56737b64a1405030a
Gabriel P. Lopes <gabriel.lopes1680@gmail.com>
2025-08-03 15:35:26 -0300

    refactor(admin): simplify event ports and adapters

--- src/main/java/org/gbl/admin/in/event/RabbitMQAdminEventListener.java
+++ src/main/java/org/gbl/admin/in/event/RabbitMQAdminEventListener.java
@@ -1,5 +1,7 @@
 package org.gbl.admin.in.event;
 
+import org.gbl.admin.FlightAdminApi;
+import org.gbl.admin.FlightAdminApi.BookSeatsRequest;
 import org.gbl.booking.event.BookingCreated;
 import org.gbl.kernel.infra.rabbitmq.RabbitMQEventListener;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,14 +11,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class RabbitMQAdminEventListener extends RabbitMQEventListener {
 
-    private final BookingCreatedHandler handler;
+    private final FlightAdminApi flightAdminApi;
 
-    public RabbitMQAdminEventListener(BookingCreatedHandler handler) {
-        this.handler = handler;
+    public RabbitMQAdminEventListener(FlightAdminApi flightAdminApi) {
+        this.flightAdminApi = flightAdminApi;
     }
 
     @RabbitListener(queues = {"${rabbit.queues.booking.created.name}"})
     public void listenToBookingCreated(@Payload String payload) {
-        createConsumerFor(payload, BookingCreated.class).accept(handler::handle);
+        createConsumerFor(payload, BookingCreated.class).accept(this::handle);
+    }
+
+    private void handle(BookingCreated event) {
+        LOG.info("Processing BookingCreated event of bookingId: {}", event.bookingId());
+        final var request = new BookSeatsRequest(event.bookingId(), event.flightId(),
+                                                 event.seatIds());
+        flightAdminApi.bookSeats(request);
     }
 }
--- src/main/java/org/gbl/kernel/application/EventHandler.java
+++
@@ -1,6 +0,0 @@
-package org.gbl.kernel.application;
-
-@FunctionalInterface
-public interface EventHandler<T extends Event> {
-    void handle(T event);
-}
